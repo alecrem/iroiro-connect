b88a710c79ea8f1b171a2d65bb3ed987854abb20
Alejandro Cremades <ale@alecrem.com>
2022-09-18 23:48:11 +0900

    Added image and formatting to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
 import type { NextPage } from 'next'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, Image, HStack, VStack } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
     <Container maxW="container.lg" mt="2em">
-      <Heading>iroiro connect</Heading>
-      <Heading size="xs">
-        人もいろいろ、必要とすることもいろいろ。個人個人を大切に思い、それぞれに必要とされるサービスを提供します。{' '}
-      </Heading>
+      <HStack spacing="24px">
+        <Image
+          src="cropped-iroiro-puzzle.png"
+          alt="iroiro connect logo"
+          htmlHeight={'64'}
+          htmlWidth={'64'}
+        />
+        <VStack alignItems="left">
+          <Heading>iroiro connect</Heading>
+          <Heading size="xs">
+            人もいろいろ、必要とすることもいろいろ。個人個人を大切に思い、それぞれに必要とされるサービスを提供します。{' '}
+          </Heading>
+        </VStack>
+      </HStack>
     </Container>
   )
 }
