f7b964454516ecb2dbccd089e61f4c471cc40457
Alejandro Cremades <ale@alecrem.com>
2023-04-13 23:31:45 +0900

    Localize header and footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,11 @@
 import { Box, Text } from '@chakra-ui/react'
+import useTranslation from 'next-translate/useTranslation'
 
 const Footer: React.FC = () => {
+  const { t } = useTranslation('common')
   return (
     <Box pt={10} pb={5} bottom={0} position="relative">
-      <Text>© iroiro connect</Text>
+      <Text>{t('footer.copyright')}</Text>
     </Box>
   )
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -22,14 +22,14 @@ const Header: FC = () => {
       <HStack spacing="24px">
         <Image
           src="/cropped-iroiro-puzzle.png"
-          alt="iroiro connect logo"
+          alt={t('header.logoalt')}
           htmlHeight={'64'}
           htmlWidth={'64'}
         />
         <VStack alignItems="left">
-          <Heading as="h1">iroiro connect</Heading>
+          <Heading as="h1">{t('header.title')}</Heading>
           <Heading as="h4" size="xs">
-            人もいろいろ、必要とすることもいろいろ。個人個人を大切に思い、それぞれに必要とされるサービスを提供します。
+            {t('header.tagline')}
           </Heading>
         </VStack>
       </HStack>
@@ -37,28 +37,28 @@ const Header: FC = () => {
         <NextLink passHref href="/ai" locale={router.locale}>
           <Link _focus={{ boxShadow: 'none' }} pr={4} pt={4} pb={4}>
             <Box as="span" display={{ base: 'none', md: 'block' }}>
-              クレマデス海上愛
+              {t('header.nav.ai')}
             </Box>
           </Link>
         </NextLink>
         <NextLink passHref href="/" locale={router.locale}>
           <Link _focus={{ boxShadow: 'none' }} href="/" p={4}>
             <Box as="span" display={{ base: 'none', md: 'block' }}>
-              移住のお手伝い
+              {t('header.nav.index')}
             </Box>
           </Link>
         </NextLink>
         <NextLink passHref href="/travel" locale={router.locale}>
           <Link _focus={{ boxShadow: 'none' }} p={4}>
             <Box as="span" display={{ base: 'none', md: 'block' }}>
-              旅行のお手伝い
+              {t('header.nav.travel')}
             </Box>
           </Link>
         </NextLink>
         <NextLink passHref href="/translation" locale={router.locale}>
           <Link _focus={{ boxShadow: 'none' }} p={4}>
             <Box as="span" display={{ base: 'none', md: 'block' }}>
-              翻訳、通訳
+              {t('header.nav.translation')}
             </Box>
           </Link>
         </NextLink>
