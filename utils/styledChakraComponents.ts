61bcceaca91af434795ce3dc7c02af0b50df43b9
Alejandro Cremades <ale@alecrem.com>
2025-03-22 08:31:49 +0900

    Remove superfluous typing

--- utils/styledChakraComponents.ts
+++ utils/styledChakraComponents.ts
@@ -3,13 +3,13 @@ import { List, ListItemProps, Text, TextProps } from '@chakra-ui/react'
 
 const StyledListItem = styled(List.Item).attrs({
   mt: '1em'
-} as ListItemProps)`
+})`
   font-size: 1.25em;
 ` as typeof List.Item
 
 const StyledText = styled(Text).attrs({
   mt: '2em'
-} as TextProps)`
+})`
   font-size: 1.25em;
 ` as typeof Text
 
